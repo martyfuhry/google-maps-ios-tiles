763f38b7e3bd2aebcd4e7773ab5cb6dbb69bde5b
Marty Fuhry <martyfuhry@gmail.com>
2024-10-03 10:16:24 -0400

    Adds broken tile to tile_overlay
    toggl
    
    Adds opaque tiles
    
    wip
    
    images

--- packages/google_maps_flutter/google_maps_flutter/example/lib/tile_overlay.dart
+++ packages/google_maps_flutter/google_maps_flutter/example/lib/tile_overlay.dart
@@ -8,6 +8,7 @@ import 'dart:typed_data';
 import 'dart:ui' as ui;
 
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 import 'page.dart';
@@ -26,128 +27,106 @@ class TileOverlayBody extends StatefulWidget {
   const TileOverlayBody({super.key});
 
   @override
-  State<StatefulWidget> createState() => TileOverlayBodyState();
+  State<TileOverlayBody> createState() => _TileOverlayBodyState();
 }
 
-class TileOverlayBodyState extends State<TileOverlayBody> {
-  TileOverlayBodyState();
-
-  GoogleMapController? controller;
-  TileOverlay? _tileOverlay;
-
-  // ignore: use_setters_to_change_properties
-  void _onMapCreated(GoogleMapController controller) {
-    this.controller = controller;
-  }
-
-  @override
-  void dispose() {
-    super.dispose();
-  }
-
-  void _removeTileOverlay() {
-    setState(() {
-      _tileOverlay = null;
-    });
-  }
-
-  void _addTileOverlay() {
-    final TileOverlay tileOverlay = TileOverlay(
-      tileOverlayId: const TileOverlayId('tile_overlay_1'),
-      tileProvider: _DebugTileProvider(),
-    );
-    setState(() {
-      _tileOverlay = tileOverlay;
-    });
-  }
-
-  void _clearTileCache() {
-    if (_tileOverlay != null && controller != null) {
-      controller!.clearTileCache(_tileOverlay!.tileOverlayId);
-    }
-  }
+class _TileOverlayBodyState extends State<TileOverlayBody> {
+  final ValueNotifier<TileOverlay?> _tileOverlay =
+      ValueNotifier<TileOverlay?>(null);
 
   @override
   Widget build(BuildContext context) {
-    final Set<TileOverlay> overlays = <TileOverlay>{
-      if (_tileOverlay != null) _tileOverlay!,
-    };
-    return Column(
-      mainAxisSize: MainAxisSize.min,
-      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-      crossAxisAlignment: CrossAxisAlignment.stretch,
-      children: <Widget>[
-        Center(
-          child: SizedBox(
-            width: 350.0,
-            height: 300.0,
-            child: GoogleMap(
-              initialCameraPosition: const CameraPosition(
-                target: LatLng(59.935460, 30.325177),
-                zoom: 7.0,
+    return ValueListenableBuilder<TileOverlay?>(
+      valueListenable: _tileOverlay,
+      builder: (BuildContext context, TileOverlay? overlay, Widget? child) {
+        final Set<TileOverlay> overlays =
+            overlay == null ? <TileOverlay>{} : <TileOverlay>{overlay};
+
+        return Column(
+          mainAxisSize: MainAxisSize.min,
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Center(
+              child: SizedBox(
+                width: 350.0,
+                height: 300.0,
+                child: GoogleMap(
+                  initialCameraPosition: const CameraPosition(
+                    target: LatLng(59.935460, 30.325177),
+                    zoom: 7.0,
+                  ),
+                  tileOverlays: overlays,
+                ),
               ),
-              tileOverlays: overlays,
-              onMapCreated: _onMapCreated,
             ),
-          ),
-        ),
-        TextButton(
-          onPressed: _addTileOverlay,
-          child: const Text('Add tile overlay'),
-        ),
-        TextButton(
-          onPressed: _removeTileOverlay,
-          child: const Text('Remove tile overlay'),
-        ),
-        TextButton(
-          onPressed: _clearTileCache,
-          child: const Text('Clear tile cache'),
-        ),
-      ],
+            TextButton.icon(
+              icon: Image.asset('assets/working_transparent_tile.png', height: 50,),
+              onPressed: () {
+                _tileOverlay.value =
+                    _getOverlay('assets/working_transparent_tile.png');
+              },
+              label: const Text(
+                'Use shaped transparent',
+              ),
+            ),
+            TextButton.icon(
+              icon: Image.asset('assets/not_working_transparent_tile.png', height: 50,),
+              onPressed: () {
+                _tileOverlay.value =
+                    _getOverlay('assets/not_working_transparent_tile.png');
+              },
+              label: const Text(
+                'Use solid (BROKEN)',
+              ),
+            ),
+            TextButton.icon(
+              icon: Image.asset('assets/working_opaque_tile.png', height: 50,),
+              onPressed: () {
+                _tileOverlay.value = _getOverlay(
+                  'assets/working_opaque_tile.png',
+                  transparency: 0.75,
+                );
+              },
+              label: const Text(
+                'Use shaped opaque with 75% transparency',
+              ),
+            ),
+            TextButton.icon(
+              icon: Image.asset('assets/not_working_opaque_tile.png', height: 50,),
+              onPressed: () {
+                _tileOverlay.value = _getOverlay(
+                  'assets/not_working_opaque_tile.png',
+                  transparency: 0.75,
+                );
+              },
+              label: const Text(
+                'Use solid opaque with 75% transpancy',
+              ),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  TileOverlay _getOverlay(String filePath, {double transparency = 0}) {
+    return TileOverlay(
+      tileOverlayId: TileOverlayId('$transparency$filePath'),
+      tileProvider: _ImageTileProvider(filePath),
+      transparency: transparency,
     );
   }
 }
 
-class _DebugTileProvider implements TileProvider {
-  _DebugTileProvider() {
-    boxPaint.isAntiAlias = true;
-    boxPaint.color = Colors.blue;
-    boxPaint.strokeWidth = 2.0;
-    boxPaint.style = PaintingStyle.stroke;
-  }
+class _ImageTileProvider implements TileProvider {
+  const _ImageTileProvider(this.imagePath);
 
-  static const int width = 100;
-  static const int height = 100;
-  static final Paint boxPaint = Paint();
-  static const TextStyle textStyle = TextStyle(
-    color: Colors.red,
-    fontSize: 20,
-  );
+  final String imagePath;
 
   @override
   Future<Tile> getTile(int x, int y, int? zoom) async {
-    final ui.PictureRecorder recorder = ui.PictureRecorder();
-    final Canvas canvas = Canvas(recorder);
-    final TextSpan textSpan = TextSpan(
-      text: '$x,$y',
-      style: textStyle,
-    );
-    final TextPainter textPainter = TextPainter(
-      text: textSpan,
-      textDirection: TextDirection.ltr,
-    );
-    textPainter.layout(
-      maxWidth: width.toDouble(),
-    );
-    textPainter.paint(canvas, Offset.zero);
-    canvas.drawRect(
-        Rect.fromLTRB(0, 0, width.toDouble(), width.toDouble()), boxPaint);
-    final ui.Picture picture = recorder.endRecording();
-    final Uint8List byteData = await picture
-        .toImage(width, height)
-        .then((ui.Image image) =>
-            image.toByteData(format: ui.ImageByteFormat.png))
-        .then((ByteData? byteData) => byteData!.buffer.asUint8List());
-    return Tile(width, height, byteData);
+    final ByteData tileImage = await rootBundle.load(imagePath);
+    return Tile(512, 512, tileImage.buffer.asUint8List());
   }
 }
